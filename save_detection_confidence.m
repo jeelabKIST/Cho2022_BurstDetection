fc91457c87cad078492e54e91d1b379943e8ec28
SungJun Cho <scho.sungjun@gmail.com>
2023-03-19 17:11:02 +0000

    Add: compute and save detection confidence scores

---
+++ save_detection_confidence.m
@@ -0,0 +1,12 @@
+%% Configure Library Paths
+util_path = genpath('/Users/scho/Neuroscience_KIST/Cho2022_BurstDetection/utils');
+data_path = genpath('/Users/scho/Neuroscience_KIST/Cho2022_BurstDetection/data');
+addpath(util_path);
+addpath(data_path);
+%% Store Detection Confidence Heatmaps
+base_path = '/Users/scho/Neuroscience_KIST/Cho2022_BurstDetection/data/simulation_data/HM_*.mat';
+band_names = {'alpha','beta','gamma'};
+for i = 1:length(band_names)
+    file_path = replace(base_path,'*',band_names{i});
+    store_detection_confidence(file_path);
+end
