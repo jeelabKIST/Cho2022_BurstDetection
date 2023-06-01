894227d18c84b7b195ee691a6f9891dc719fa145
SungJun Cho <scho.sungjun@gmail.com>
2023-06-01 11:12:55 +0100

    fix: frequency band name

--- save_detection_confidence.m
+++ save_detection_confidence.m
@@ -5,7 +5,7 @@ addpath(util_path);
 addpath(data_path);
 %% Store Detection Confidence Heatmaps
 base_path = '/Users/scho/Neuroscience_KIST/Cho2022_BurstDetection/data/simulation_data/HM_*.mat';
-band_names = {'alpha','beta','gamma'};
+band_names = {'theta','beta','gamma'};
 for i = 1:length(band_names)
     file_path = replace(base_path,'*',band_names{i});
     store_detection_confidence(file_path);
